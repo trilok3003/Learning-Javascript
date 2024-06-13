2ad26202b6d85962dbc7693f9a427414b80b8393
trilok <trilok@gaiasys.in>
2024-06-13 15:16:50 +0530

    wip

---
+++ javascript/lexical-Scope/lexical-Scope.js
@@ -0,0 +1,15 @@
+const myFunction = () => {
+    let myValue = 2;
+    console.log(myValue);
+
+    const childFunction = () => {
+         console.log(myValue += 1);
+    }
+
+    childFunction();
+}
+
+myFunction();
+myFunction();
+myFunction();
+
